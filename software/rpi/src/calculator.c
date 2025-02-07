b87f2b898d0fbc4befc0f47a0a5e20d7a96f8cc7
Mischa Alff <mischa@storyweavers.io>
2025-02-07 09:33:21 +0100

    Make calculator functional

--- software/rpi/src/calculator.c
+++ software/rpi/src/calculator.c
@@ -7,69 +7,158 @@
 #include "ui.h"
 #include "calculator.h"
 
+static void reset_scratch(calculator_t *calculator) {
+	calculator->scratch_index = 0;
+	calculator->scratch[calculator->scratch_index] = '0';
+	calculator->scratch[calculator->scratch_index+1] = 0;
+}
+
 static void input_1(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 1;
+	calculator->scratch[calculator->scratch_index++] = '1';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_2(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 2;
+	calculator->scratch[calculator->scratch_index++] = '2';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_3(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 3;
+	calculator->scratch[calculator->scratch_index++] = '3';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_4(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 4;
+	calculator->scratch[calculator->scratch_index++] = '4';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_5(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 5;
+	calculator->scratch[calculator->scratch_index++] = '5';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_6(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 6;
+	calculator->scratch[calculator->scratch_index++] = '6';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_7(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 7;
+	calculator->scratch[calculator->scratch_index++] = '7';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_8(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 8;
+	calculator->scratch[calculator->scratch_index++] = '8';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_9(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
-	calculator->scratch += 9;
+	calculator->scratch[calculator->scratch_index++] = '9';
+	calculator->scratch[calculator->scratch_index] = 0;
 }
 
 static void input_0(int column, int row, void *user_ptr) {
 	calculator_t *calculator = user_ptr;
-	calculator->scratch *= 10;
+	calculator->scratch[calculator->scratch_index++] = '0';
+	calculator->scratch[calculator->scratch_index] = 0;
+}
+
+static void input_decimal(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	calculator->scratch[calculator->scratch_index++] = '.';
+	calculator->scratch[calculator->scratch_index] = 0;
+}
+
+static void clear_all(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	reset_scratch(calculator);
+	calculator->operand = 0.f;
+	calculator->result = 0.f;
+	calculator->operation = CALCULATOR_OPERATION_NONE;
+}
+
+static void clear_entry(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	reset_scratch(calculator);
+}
+
+static void input_del(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	if (calculator->scratch_index > 0) {
+		calculator->scratch[--calculator->scratch_index] = 0;
+	}
+}
+
+static void submit_operation(calculator_t *calculator) {
+	sscanf_s(calculator->scratch, "%f", &calculator->operand);
+	switch (calculator->operation) {
+	case CALCULATOR_OPERATION_ADD: {
+		calculator->result += calculator->operand;
+	} break;
+	case CALCULATOR_OPERATION_SUB: {
+		calculator->result -= calculator->operand;
+	} break;
+	case CALCULATOR_OPERATION_MUL: {
+		calculator->result *= calculator->operand;
+	} break;
+	case CALCULATOR_OPERATION_DIV: {
+		calculator->result /= calculator->operand;
+	} break;
+	}
+	reset_scratch(calculator);
+}
+
+static void input_add(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	submit_operation(calculator);
+	calculator->operation = CALCULATOR_OPERATION_ADD;
+	reset_scratch(calculator);
+}
+
+static void input_sub(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	submit_operation(calculator);
+	calculator->operation = CALCULATOR_OPERATION_SUB;
+	reset_scratch(calculator);
+}
+
+static void input_mul(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	submit_operation(calculator);
+	calculator->operation = CALCULATOR_OPERATION_MUL;
+	reset_scratch(calculator);
+}
+
+static void input_div(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	submit_operation(calculator);
+	calculator->operation = CALCULATOR_OPERATION_DIV;
+	reset_scratch(calculator);
+}
+
+static void input_enter(int column, int row, void *user_ptr) {
+	calculator_t *calculator = user_ptr;
+	submit_operation(calculator);
+	calculator->operation = CALCULATOR_OPERATION_NONE;
+	reset_scratch(calculator);
 }
 
 void init_calculator(calculator_t *calculator, dro_t *dro) {
 	calculator->dro = dro;
 	calculator->operand = 0.f;
-	calculator->scratch = 0.f;
+	calculator->result = 0.f;
+	calculator->operation = CALCULATOR_OPERATION_ADD;
+	reset_scratch(calculator);
 
 	init_keypad(&calculator->keypad, 5, 5, 80, 80);
 
@@ -82,10 +171,14 @@ void init_calculator(calculator_t *calculator, dro_t *dro) {
 
 	{
 		calculator->keypad.buttons[0][0].button_text = "C";
+		calculator->keypad.buttons[0][0].callback = clear_all;
 		calculator->keypad.buttons[1][0].button_text = "CE";
+		calculator->keypad.buttons[0][0].callback = clear_entry;
 		calculator->keypad.buttons[2][0].button_text = "DEL";
+		calculator->keypad.buttons[2][0].callback = input_del;
 		calculator->keypad.buttons[3][0].button_text = NULL;
 		calculator->keypad.buttons[4][0].button_text = "+";
+		calculator->keypad.buttons[4][0].callback = input_add;
 		calculator->keypad.buttons[0][1].button_text = "1";
 		calculator->keypad.buttons[0][1].callback = input_1;
 		calculator->keypad.buttons[1][1].button_text = "2";
@@ -94,6 +187,7 @@ void init_calculator(calculator_t *calculator, dro_t *dro) {
 		calculator->keypad.buttons[2][1].callback = input_3;
 		calculator->keypad.buttons[3][1].button_text = NULL;
 		calculator->keypad.buttons[4][1].button_text = "-";
+		calculator->keypad.buttons[4][1].callback = input_sub;
 		calculator->keypad.buttons[0][2].button_text = "4";
 		calculator->keypad.buttons[0][2].callback = input_4;
 		calculator->keypad.buttons[1][2].button_text = "5";
@@ -102,6 +196,7 @@ void init_calculator(calculator_t *calculator, dro_t *dro) {
 		calculator->keypad.buttons[2][2].callback = input_6;
 		calculator->keypad.buttons[3][2].button_text = NULL;
 		calculator->keypad.buttons[4][2].button_text = "x";
+		calculator->keypad.buttons[4][2].callback = input_mul;
 		calculator->keypad.buttons[0][3].button_text = "7";
 		calculator->keypad.buttons[0][3].callback = input_7;
 		calculator->keypad.buttons[1][3].button_text = "8";
@@ -110,16 +205,30 @@ void init_calculator(calculator_t *calculator, dro_t *dro) {
 		calculator->keypad.buttons[2][3].callback = input_9;
 		calculator->keypad.buttons[3][3].button_text = NULL;
 		calculator->keypad.buttons[4][3].button_text = "/";
+		calculator->keypad.buttons[4][3].callback = input_div;
+		calculator->keypad.buttons[0][4].button_text = NULL;
 		calculator->keypad.buttons[1][4].button_text = "0";
 		calculator->keypad.buttons[1][4].callback = input_0;
 		calculator->keypad.buttons[2][4].button_text = ".";
+		calculator->keypad.buttons[2][4].callback = input_decimal;
+		calculator->keypad.buttons[3][4].button_text = NULL;
+		calculator->keypad.buttons[4][4].button_text = "ENTER";
+		calculator->keypad.buttons[4][4].callback = input_enter;
 	}
 }
 
 void draw_calculator(calculator_t *calculator, ui_t *ui, int offset_x, int offset_y) {
 	ui_rect_t rect = {.x = offset_x, .y = offset_y};
+
 	char buf[16];
-	snprintf(buf, 16, "% .2f", calculator->scratch);
+
+	// if scratch is empty, show result
+	if (calculator->scratch_index == 0) {
+		snprintf(buf, 16, "% 08.2f", calculator->result);
+	} else {
+		snprintf(buf, 16, "%s", calculator->scratch);
+	}
+
 	ui_text(ui, rect, buf);
 
 	draw_keypad(&calculator->keypad, ui, offset_x, offset_y + SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * 2.f + 10.f);
--- software/rpi/src/calculator.h
+++ software/rpi/src/calculator.h
@@ -3,14 +3,29 @@
 
 #include "keypad.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+
 typedef struct dro_t dro_t;
 typedef struct ui_t ui_t;
+
+enum {
+	CALCULATOR_OPERATION_NONE = 0,
+	CALCULATOR_OPERATION_ADD,
+	CALCULATOR_OPERATION_SUB,
+	CALCULATOR_OPERATION_MUL,
+	CALCULATOR_OPERATION_DIV,
+};
 
 typedef struct calculator_t {
 	dro_t *dro;
 	keypad_grid_t keypad;
 	float operand;
-	float scratch;
+	float result;
+	uint32_t operation;
+	char scratch[64];
+	uint32_t scratch_index;
+	bool scratch_sign;
 } calculator_t;
 
 void init_calculator(calculator_t *calculator, dro_t *dro);
